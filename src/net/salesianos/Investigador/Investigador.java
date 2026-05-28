3c3da4091a3da77c197dc0005335331b2e29d415
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-05-28 13:51:43 +0200

    feat: Clase abstracta Investigador y sus atributos

---
+++ src/net/salesianos/Investigador/Investigador.java
@@ -0,0 +1,9 @@
+package net.salesianos.Investigador;
+
+public abstract class Investigador {
+
+    protected String nombre;        
+    protected String especialidad;  
+    protected double sueldo;        
+    
+}
