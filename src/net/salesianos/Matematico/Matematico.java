eb99adc9e2839077b378fa098e976950e23a2ed6
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-05-28 23:47:51 +0200

    feat: clase Metodos

--- src/net/salesianos/Matematico/Matematico.java
+++ src/net/salesianos/Matematico/Matematico.java
@@ -1,16 +1,17 @@
 package net.salesianos.Matematico;
+
 import net.salesianos.Investigador.Investigador;
 
 public class Matematico extends Investigador {
 
     private int[][] matriz;
 
-     public Matematico(String nombre, double sueldo, int[][] matriz) {
+    public Matematico(String nombre, double sueldo, int[][] matriz) {
         super(nombre, "Matemático", sueldo); // especialidad fija
         this.matriz = matriz;
     }
 
-     @Override
+    @Override
     public String estadoSueldo() {
 
         return sueldo < 1800 ? "Debe pedir aumento." : "Sueldo correcto.";
@@ -21,5 +22,31 @@ public class Matematico extends Investigador {
         System.out.println("Mostrando matriz actual:");
         mostrarMatriz();
     }
-    
+
+    public void trabajar(int n) {
+        System.out.println("Multiplicando matriz por " + n);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matriz[i][j] *= n;
+            }
+        }
+    }
+
+    public void trabajar(int[][] otra) {
+        System.out.println("Sumando matrices...");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matriz[i][j] += otra[i][j];
+            }
+        }
+    }
+
+    private void mostrarMatriz() {
+        for (int[] fila : matriz) {
+            for (int n : fila) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
+    }
 }
