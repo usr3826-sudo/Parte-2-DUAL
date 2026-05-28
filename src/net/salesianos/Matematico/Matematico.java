5f67a19642d7088f5f24a2331e8a3fd4dcd92449
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-05-28 22:29:53 +0200

    feat: Constructor

--- src/net/salesianos/Matematico/Matematico.java
+++ src/net/salesianos/Matematico/Matematico.java
@@ -4,4 +4,9 @@ import net.salesianos.Investigador.Investigador;
 public class Matematico extends Investigador {
 
     private int[][] matriz;
+
+     public Matematico(String nombre, double sueldo, int[][] matriz) {
+        super(nombre, "Matemático", sueldo); // especialidad fija
+        this.matriz = matriz;
+    }
 }
