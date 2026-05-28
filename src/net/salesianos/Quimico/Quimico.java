02f75b02c0fd86f69d24ac54b2dc143029394401
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-05-29 01:39:39 +0200

    feat: Clase Quimico entera

--- src/net/salesianos/Quimico/Quimico.java
+++ src/net/salesianos/Quimico/Quimico.java
@@ -1,5 +1,41 @@
 package net.salesianos.Quimico;
 
-public class Quimico {
-    
+import net.salesianos.Investigador.Investigador;
+import net.salesianos.TrabajoDual.TrabajoDual;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Quimico extends Investigador implements TrabajoDual {
+
+    private ArrayList<String> elementos = new ArrayList<>();
+
+    public Quimico(String nombre, double sueldo) {
+        super(nombre, "Químico", sueldo);
+    }
+
+    public void anadir(String e) {
+        elementos.add(e);
+    }
+
+    @Override
+    public String estadoSueldo() {
+        return sueldo < 1200 ? "Debe pedir aumento." : "Sueldo correcto.";
+    }
+
+    @Override
+    public void trabajar() {
+        System.out.println("Químico trabajando → invirtiendo elementos");
+        Collections.reverse(elementos);
+    }
+
+    @Override
+    public void trabajoDual() {
+        System.out.println("Químico en trabajo dual → ordenando y eliminando mayor");
+        elementos.sort((a, b) -> a.length() - b.length());
+        elementos.remove(elementos.size() - 1);
+    }
+
+    public ArrayList<String> getElementos() {
+        return elementos;
+    }
 }
