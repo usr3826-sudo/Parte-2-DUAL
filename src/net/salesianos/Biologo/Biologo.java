aab582a27215f84730bbb7aafeab27b3ff2b83b2
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-05-28 23:56:48 +0200

    feat: Clase Biologo, herencia Investrigador, e implemntacion TrabajoDual, Arraylist y constructor

--- src/net/salesianos/Biologo/Biologo.java
+++ src/net/salesianos/Biologo/Biologo.java
@@ -1,5 +1,16 @@
 package net.salesianos.Biologo;
 
-public class Biologo {
-    
+import net.salesianos.Investigador.Investigador;
+import net.salesianos.TrabajoDual.TrabajoDual;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Biologo extends Investigador implements TrabajoDual {
+
+    private ArrayList<String> especimenes = new ArrayList<>();
+
+    public Biologo(String nombre, double sueldo) {
+        super(nombre, "Biólogo", sueldo);
+    }
+
 }
